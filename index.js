143220c81efa41cfba632e1f2459dcfa308b7bcf
ferkerhik <73007558+ferkerhik@users.noreply.github.com>
2023-06-01 23:02:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.delete("/:table/:ids", async (req, res) => {
     });
   } catch (error) {
     console.error("Error deleting data:", error);
-    res.status(500).send({ status: "error", message: error });
+    res.status(500).send({ status: "error", message: error.message || 'Something went wrong' });
   }
 });
 
